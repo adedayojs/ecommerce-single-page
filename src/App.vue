8e3632fbeffb925507bb42eb28682fe792bc8378
Adedayo Adedunye <samfeolu@Adedayos-MacBook-Pro.local>
2019-11-16 00:36:56 +0100

    Deletion: Removed all Initial Template to start afresh

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,13 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <div></div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
   }
 }
 </script>
